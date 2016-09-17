a4dc1fe10037663e9aae8a648110b1fdc6abb087
hui-w <huiwang@qlike.com>
2016-09-17 14:38:34 +0800

    Migrate DateDiff from SVN

---
+++ DateDiff/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Date Diff</title>
+
+    <script type="text/javascript" src="calendarDateInput.js"></script>
+
+    <link rel="stylesheet" type="text/css" media="all" href="datediff.css" />
+</head>
+<body>
+    <form id="frmMain" action="Default.aspx">
+        <div>
+            <div class="row">
+                <label>
+                    Date <span class="u">F</span>rom:</label>
+                <asp:Literal ID="litDateFrom" runat="server" EnableViewState="false"></asp:Literal>
+            </div>
+            <div class="row">
+                <label>
+                    Add <span class="u">D</span>ay(s):</label>
+                <asp:Literal ID="litDays" runat="server" EnableViewState="false"></asp:Literal>
+            </div>
+            <div class="row">
+                <label>
+                    Date <span class="u">T</span>o:</label>
+                <asp:Literal ID="litDateTo" runat="server" EnableViewState="false"></asp:Literal>
+            </div>
+            <div class="row">
+                <label>
+                    &nbsp;</label>
+                <input type="submit" value="Calculate" class="button" />
+            </div>
+            <asp:Literal ID="litOutput" runat="server" EnableViewState="false"></asp:Literal>
+        </div>
+        <div class="copyright">
+            DateDiff WebApp, Copyright &copy 2002-<asp:Literal ID="litCopyYear" runat="server" EnableViewState="false"></asp:Literal> QLike.com
+        </div>
+    </form>
+</body>
+</html>
---
+++ DateDiff/Default.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DateTime from = DateTime.MinValue;
+        DateTime to = DateTime.MinValue;
+        int days = 0;
+
+        #region Get the input
+        if (Request.QueryString["F"] != null)
+        {
+            if (DateTime.TryParse(Request.QueryString["F"], out from))
+            {
+                Int32.TryParse(Request.QueryString["D"], out days);
+                DateTime.TryParse(Request.QueryString["T"], out to);
+            }
+        }
+        #endregion
+
+        #region Date From
+        if (from.Equals(DateTime.MinValue))
+        {
+            this.litDateFrom.Text = "<script type=\"text/javascript\">DateInput('F', true, 'YYYY-MM-DD')</script>";
+        }
+        else
+        {
+            string format = "<script type=\"text/javascript\">DateInput('F', true, 'YYYY-MM-DD', '{0}')</script>";
+            this.litDateFrom.Text = string.Format(format, this.FormatDateTime(from));
+        }
+        #endregion
+
+        #region Days
+        if (days == 0)
+        {
+            this.litDays.Text = "<input type=\"text\" name=\"D\" class=\"textbox\" accesskey=\"d\" maxlength=\"6\" />";
+        }
+        else
+        {
+            string format = "<input type=\"text\" name=\"D\" value=\"{0}\" class=\"textbox\" accesskey=\"d\" maxlength=\"6\" />";
+            this.litDays.Text = string.Format(format, days.ToString());
+        }
+        #endregion
+
+        #region Date To
+        if (to.Equals(DateTime.MinValue))
+        {
+            this.litDateTo.Text = "<script type=\"text/javascript\">DateInput('T', false, 'YYYY-MM-DD')</script>";
+        }
+        else
+        {
+            string format = "<script type=\"text/javascript\">DateInput('T', false, 'YYYY-MM-DD', '{0}')</script>";
+            this.litDateTo.Text = string.Format(format, this.FormatDateTime(to));
+        }
+        #endregion To
+
+        #region Calc and Output
+        if (!from.Equals(DateTime.MinValue))
+        {
+            bool showOutput = false;
+            StringBuilder sbOutput = new StringBuilder("<div class=\"output\">");
+            if (!days.Equals(0))
+            {
+                try
+                {
+                    DateTime dt = from.AddDays((double)days);
+                    string format = "<div><span class=\"variable\">{0}</span> day(s) after <span class=\"source\">{1}</span> is <span class=\"result\">{2}</span></div>";
+                    sbOutput.AppendFormat(format, days.ToString(), this.FormatDateTime(from), this.FormatDateTime(dt));
+                    showOutput = true;
+                }
+                catch { }
+            }
+
+            if (!to.Equals(DateTime.MinValue))
+            {
+                try
+                {
+                    TimeSpan ts = to.Subtract(from);
+                    string format = "<div><span class=\"variable\">{0}</span> is <span class=\"result\">{1}</span> day(s) before <span class=\"source\">{2}</span></div>";
+                    sbOutput.AppendFormat(format, this.FormatDateTime(from), ts.Days.ToString(), this.FormatDateTime(to));
+                    showOutput = true;
+                }
+                catch { }
+            }
+            sbOutput.AppendLine("</div>");
+
+            this.litOutput.Text = showOutput ? sbOutput.ToString() : string.Empty;
+        }
+        #endregion
+
+        this.litCopyYear.Text = DateTime.Now.Year.ToString();
+    }
+
+    /// <summary>
+    /// Format the datetime
+    /// </summary>
+    /// <param name="dt"></param>
+    /// <returns></returns>
+    protected string FormatDateTime(DateTime dt)
+    {
+        return string.Format("{0:yyyy-MM-dd}", dt);
+    }
+}
